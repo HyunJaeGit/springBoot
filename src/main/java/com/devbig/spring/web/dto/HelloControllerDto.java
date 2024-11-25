e7bd9966aef69d1e9b3a3ea5ccb197c6154627ad
guswo1118 <guswo1118@naver.com>
2024-11-26 01:16:04 +0900

    feat : add HelloControllerDto with lombok annotations test : add unit test for HelloResponseDtoTest fix : downgrade gradle version to 4.10.2 for compatibility

---
+++ src/main/java/com/devbig/spring/web/dto/HelloControllerDto.java
@@ -0,0 +1,21 @@
+package com.devbig.spring.web.dto;
+
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+/*
+    1. Getter : 선언된 모든 필드의 get 메소드를 생성
+    2. RequiredArgsConstructor
+        - 선언된 모든 final 필드가 포함된 생성자를 생성
+        - final이 없는 필드는 생성자에 포함되지 않음
+*/
+
+@Getter
+@RequiredArgsConstructor
+public class HelloControllerDto {
+
+    private final String name;
+    private final int amount;
+
+}
