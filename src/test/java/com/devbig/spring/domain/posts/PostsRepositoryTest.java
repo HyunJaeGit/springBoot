3598a42e420fb6a8d4c8047e46a9b8e99b01d243
guswo1118 <guswo1118@naver.com>
2024-12-09 22:36:59 +0900

    test : Test class PostRepositoryTest

--- src/test/java/com/devbig/spring/domain/posts/PostsRepositoryTest.java
+++ src/test/java/com/devbig/spring/domain/posts/PostsRepositoryTest.java
@@ -1,5 +1,14 @@
 package com.devbig.spring.domain.posts;
 
+
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+
+// - save, findAll 기능 테스트
+@RunWith(SpringRunner.class)
+@SpringBootTest
 public class PostsRepositoryTest {
 
 
