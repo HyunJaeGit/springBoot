41f64be485d69f6e98f038caafaae10615cfac17
guswo1118 <guswo1118@naver.com>
2024-12-21 22:46:19 +0900

    add : controller : PostsApiController
    - PostMapping save()

--- src/main/java/com/devbig/spring/web/PostsApiController.java
+++ src/main/java/com/devbig/spring/web/PostsApiController.java
@@ -1,4 +1,21 @@
 package com.devbig.spring.web;
 
+
+import com.devbig.spring.service.posts.PostsService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
 public class PostsApiController {
+
+    private  final PostsService postsService;
+
+    @PostMapping()
+    public Long save() {
+
+    }
+
+
 }
