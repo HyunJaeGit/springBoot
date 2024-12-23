34e318a743d91540e719a48ae055e52058f1227d
guswo1118 <guswo1118@naver.com>
2024-12-23 21:57:56 +0900

    feat : implement save() method in PostsApiController

--- src/main/java/com/devbig/spring/web/PostsApiController.java
+++ src/main/java/com/devbig/spring/web/PostsApiController.java
@@ -2,8 +2,10 @@ package com.devbig.spring.web;
 
 
 import com.devbig.spring.service.posts.PostsService;
+import com.devbig.spring.web.dto.PostsSaveRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RequiredArgsConstructor
@@ -12,9 +14,9 @@ public class PostsApiController {
 
     private  final PostsService postsService;
 
-    @PostMapping()
-    public Long save() {
-
+    @PostMapping("/api/v1/posts")
+    public Long save(@RequestBody PostsSaveRequestDto requestDto) {
+        return postsService.save(requestDto);
     }
 
 
