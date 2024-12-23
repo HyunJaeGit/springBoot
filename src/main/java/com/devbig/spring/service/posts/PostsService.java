1863847c76de6205b5ee31b25f8267b51bbbdb75
guswo1118 <guswo1118@naver.com>
2024-12-23 22:14:52 +0900

    feat : add constructor injection for postsRepository in PostsService

--- src/main/java/com/devbig/spring/service/posts/PostsService.java
+++ src/main/java/com/devbig/spring/service/posts/PostsService.java
@@ -1,4 +1,21 @@
 package com.devbig.spring.service.posts;
 
+
+import com.devbig.spring.domain.posts.PostsRepository;
+import com.devbig.spring.web.dto.PostsSaveRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+
+@RequiredArgsConstructor
+@Service
 public class PostsService {
+    private final PostsRepository postsRepository;
+
+    @Transactional
+    public Long save(PostsSaveRequestDto requestDto) {
+        return postsRepository.save(requestDto.toEntity()).getId();
+    }
+
 }
--- src/main/java/com/devbig/spring/web/dto/PostsSaveRequestDto.java
+++ src/main/java/com/devbig/spring/web/dto/PostsSaveRequestDto.java
@@ -1,4 +1,11 @@
 package com.devbig.spring.web.dto;
 
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
 public class PostsSaveRequestDto {
+
 }
